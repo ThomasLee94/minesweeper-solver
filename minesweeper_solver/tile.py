af0abc216b4e4c6d19b974e93c12c27203f8e434
Thomas Lee <thomaslee9474@gmail.com>
2021-08-17 16:52:53 -0700

    [add] dunders

--- minesweeper_solver/tile.py
+++ minesweeper_solver/tile.py
@@ -1,11 +1,51 @@
 class Tile:
-    def __init__(self, coords):
+    def __init__(self, coords, num_adjacent_mines=0, is_mine=False):
         i, j = coords
 
         self.i = i
         self.j = j
 
+        self.num_adjacent_mines = num_adjacent_mines
+        self._is_mine = is_mine
+
+    def __repr__(self):
+        return f'Tile(coords={self.i, self.j}, n={self.num_adjacent_mines}, b={self.is_mine()})'
+    
+    def __str__(self):
+        if self.is_mine():
+            return '\033[91m*\033[0m'
+        elif self.is_blank():
+            # blank visible tile
+            return ' '
+        else:
+            ENDC = '\033[0m'
+            colors = [
+                None,
+                '\033[92m',  # 1 green
+                '\033[94m',  # 2 blue
+                '\033[91m',  # 3 red
+                '\033[95m',  # 4 pink/purple
+                '\033[96m',  # 5+ light blue
+            ]
+            color = colors[min(self.num_adjacent_mimes, 5)]
+            return f'{color}{self.num_adjacent_mines}{ENDC}'
+    
+    def __hash__(self):
+        return hash((self.i, self.j))
+    
+    def __eq__(self, other_tile):
+        return id(self) == id(other_tile)
+
     def coordinates(self):
         return self.i, self.j
+    
+    def is_mine(self):
+        pass
+
+    def is_blank(self):
+        pass
+
+    def make_mine(self):
+        pass
 
     
